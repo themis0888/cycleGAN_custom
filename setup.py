89fbddfd6c26610d1b1a98a6dac4dc322721ee9f
themis0888 <themis0888@gmail.com>
2018-08-11 03:54:32 +0900

    Custom version init

---
+++ setup.py
@@ -0,0 +1,15 @@
+#nsml: tensorflow/tensorflow:latest
+
+from distutils.core import setup
+setup(
+        name='nsml example 10 ladder_network',
+        version='1.0',
+        description='ns-ml',
+        install_requires=[
+            'matplotlib',
+            'tqdm',
+            'pillow',
+            'scipy',
+            'numpy'
+        ]
+)
